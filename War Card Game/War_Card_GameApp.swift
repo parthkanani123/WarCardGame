91aba7ad614f190e19ee113fb0e53f60a4b2b723
parthkanani123 <parthkanani40@gmail.com>
2024-02-28 19:29:36 +0530

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by parth kanani on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
